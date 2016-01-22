c48bf3744a172f5ecd4fdd41cecd49fa90e26b74
Ehud Cohen <ehudco@gmail.com>
2016-01-22 07:41:06 +0000

    EEPROM_sys.cpp created online with Bitbucket

---
+++ receiver/libraries/EEPROM_sys/EEPROM_sys.cpp
@@ -0,0 +1,47 @@
+#include "Arduino.h"
+#include "EEPROM_sys.h"
+
+void EEPROM_sys::EEPROM_sys(void)
+{
+	size=EEPROM.length();
+}
+
+bool EEPROM_sys::read(ComVector &vec, char idx) 
+{
+	int offset,i,*p,len;
+	
+	len=vec.GetLength();
+	p=vec.GetVec();
+	
+	offset = calc_offset(len,idx);
+	if (offset<0) return(false);
+	else
+	{
+		for(i=0;i<len,i++) p[i]=EEPROM.get(offset+i*sizeof(int));
+		return(true);
+	}
+}
+
+bool EEPROM_sys::write(ComVector &vec, char idx) 
+{
+	int offset,i,*p,len;
+	
+	len=vec.GetLength();
+	p=vec.GetVec();
+	
+	offset = calc_offset(len,idx);
+	if (offset<0) return(false);
+	else
+	{
+		for(i=0;i<len,i++) EEPROM.put(offset+i*sizeof(int),p[i]);
+		return(true);
+	}
+}
+
+
+int EEPROM_sys::calc_offset(int len, char idx) 
+{
+	if (((idx+1)*len*sizeof(int))<=this.size) return(idx*len*sizeof(int));
+	else return(-1); //no space for this table
+}
+
