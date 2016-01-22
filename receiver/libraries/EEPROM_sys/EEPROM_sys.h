7cdf4b0762efbb81d14c205018171a231165a0ae
Ehud Cohen <ehudco@gmail.com>
2016-01-22 07:41:41 +0000

    EEPROM_sys.h created online with Bitbucket

---
+++ receiver/libraries/EEPROM_sys/EEPROM_sys.h
@@ -0,0 +1,19 @@
+#ifndef EEPROM_sys_h
+#define EEPROM_sys_h
+
+#include "Arduino.h"
+#include <EEPROM.h>
+#include "ComVector.h"
+
+class EEPROM_sys
+{
+	public:
+		EEPROM_sys(void);
+		bool write(ComVector &vec, char idx) ;
+		bool read((ComVector &vec, char idx) ;
+		
+	private:
+		int size;
+		int calc_offset(int len, char idx);
+}
+#endif
