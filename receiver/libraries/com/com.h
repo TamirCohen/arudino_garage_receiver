07af7ae19c5f2e27317bd590bf49c6bb6cef9de2
tamir cohen <tamir@tamirc.com>
2016-01-22 07:46:35 +0000

    com.h created online with Bitbucket

---
+++ receiver/libraries/com/com.h
@@ -0,0 +1,14 @@
+#ifndef com_h
+#define com_h
+#include "Arduino.h" 
+class com
+  {
+    private:
+      ComVector _ArrVec[2]; 
+    public:
+      void MultiRead(void);
+      ComVector* GetCode(void);
+       
+    
+  };
+#endif
