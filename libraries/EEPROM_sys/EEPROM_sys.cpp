2d24546ad617fc808f3664a3871ab23fb3488e7e
Tamir <tamir@tamirc.com>
2016-01-24 21:22:52 +0200

    added in eprom length of vector before the data

--- libraries/EEPROM_sys/EEPROM_sys.cpp
+++ libraries/EEPROM_sys/EEPROM_sys.cpp
@@ -1,6 +1,8 @@
 #include "Arduino.h"
 #include "EEPROM_sys.h"
 
+#define max_vec_len 200
+
 EEPROM_sys::EEPROM_sys(void)
 {
 	size=EEPROM.length();
@@ -10,19 +12,19 @@ bool EEPROM_sys::read(ComVector &vec, char idx)
 {
 	int offset,i,*p,len;
 	
-	len=vec.GetLength();
 	p=vec.GetVec();
 	
-	offset = calc_offset(len,idx);
-	if (offset<0) return(false);
+	offset = calc_offset(max_vec_len,idx);
+	if (offset<0) return(-1);
 	else
-	{
-		for(i=0;i<len;i++)
+	{	
+		EEPROM.get(offset,len);
+		for(i=0;i<max_vec_len;i++)
 		{
-			EEPROM.get(offset+i*sizeof(int),p[i]);
+			EEPROM.get(offset+(i+1)*sizeof(int),p[i]);
 		}			
 
-		return(true);
+		return(len);
 	}
 }
 
@@ -31,13 +33,15 @@ bool EEPROM_sys::write(ComVector &vec, char idx)
 	int offset,i,*p,len;
 	
 	len=vec.GetLength();
+	if (len>max_vec_len) return(false);
 	p=vec.GetVec();
 	
-	offset = calc_offset(len,idx);
+	offset = calc_offset(max_vec_len,idx);
 	if (offset<0) return(false);
 	else
 	{
-		for(i=0;i<len;i++) EEPROM.put(offset+i*sizeof(int),p[i]);
+		EEPROM.put(offset+i,len);
+		for(i=0;i<len;i++) EEPROM.put(offset+(i+1)*sizeof(int),p[i]);
 		return(true);
 	}
 }
@@ -45,9 +49,9 @@ bool EEPROM_sys::write(ComVector &vec, char idx)
 
 int EEPROM_sys::calc_offset(int len, char idx) 
 {
-	if (((idx+1)*len*sizeof(int)) <= size) 
+	if (((idx+1)*(len+1)*sizeof(int)) <= size) 
 	{
-		return(idx*len*sizeof(int));
+		return(idx*(len+1)*sizeof(int));
 	}	
 	
 	else return(-1); //no space for this table
