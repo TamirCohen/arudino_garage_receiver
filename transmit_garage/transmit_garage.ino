ee10e483bd0c1d7c9795708b8411affee4718b22
TamirCohen <tamir@tamirc.com>
2016-09-15 09:42:41 +0300

    found an transmit garage that was on desktop looks newer

--- transmit_garage/transmit_garage.ino
+++ transmit_garage/transmit_garage.ino
@@ -25,35 +25,25 @@
 
 #define startbit_low 4000
 #define startbit_high 1500
-#define one_logic_low 1200
-#define one_logic_high 300
-#define zero_logic_low 600
-#define zero_logic_high 900
+#define one_logic_low 600
+#define one_logic_high 170
+#define zero_logic_low 300
+#define zero_logic_high 450
 
  // Analog output pin that the LED is attached to
 char col_count=0;
 bool pin_state=true;
 int i=0;
 int j=0;
-char code[N]={
- 1,1,1,1,
- 1,1,1,1,
- 0,1,1,0,
- 1,1,1,0,
- 1,0,1,1,
- 0,1,0,0,
- 0,1,1,0,
- 0,1,1,1,
- 1,1,1,1,
- 0,0,0,1,
- 0,0,1,1,
- 1,1,0,1,
- 1,0,0,0};
+char code[N]={1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,0,1,0,1,1,0,1,0,0,0,1,1,0,0,1,1,1,1,1,1,1,0,0,0,1,0,0,1,1,1,1,0,1,1,0,0,0};
 
 //void (*StFuns[2]){s0,s1};
 void setup() {
- 
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+  pinMode(3,INPUT);
   pinMode(5,OUTPUT);
+  pinMode(4,OUTPUT);
   pinMode(13,OUTPUT);
 }
 
@@ -62,31 +52,34 @@ void loop() {
 
   j=0;
 
+  digitalWrite(13,HIGH); //LED
   
-  
-  digitalWrite(5,LOW);
+  digitalWrite(4,LOW);
   delayMicroseconds(startbit_low);
   
-  digitalWrite(5,HIGH);
+  digitalWrite(4,HIGH);
   delayMicroseconds(startbit_high);
   
-  while (j<N) {  
+  while (j<N) {
+    digitalWrite(5,pin_state);
+    pin_state=!pin_state;   
     if (code[j]) {
-      digitalWrite(5,LOW);
+      digitalWrite(4,LOW);
       delayMicroseconds(one_logic_low);
-      digitalWrite(5,HIGH);
+      digitalWrite(4,HIGH);
       delayMicroseconds(one_logic_high);
       
-    } else {
-      digitalWrite(5,LOW);
+    }
+    else 
+    {
+      digitalWrite(4,LOW);
       delayMicroseconds(zero_logic_low);
-      digitalWrite(5,HIGH);
+      digitalWrite(4,HIGH);
       delayMicroseconds(zero_logic_high);      
     }
     j++;
   }
-  digitalWrite(13,HIGH); //LED
-  delay(500);
+  digitalWrite(4,LOW);
   digitalWrite(13,LOW);  //LED
-  delay(500); 
+  delay(100); 
 }
