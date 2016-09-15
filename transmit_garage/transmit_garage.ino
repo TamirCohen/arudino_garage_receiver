0f4c5e409e473fc865b117aa8dff4920c71eefaf
TamirCohen <tamir@tamirc.com>
2016-09-15 09:51:48 +0300

    code cleanup, and changed pin 4 to 5 Tested - and opens the door

--- transmit_garage/transmit_garage.ino
+++ transmit_garage/transmit_garage.ino
@@ -1,24 +1,3 @@
-/*
-  Analog input, analog output, serial output
-
- Reads an analog input pin, maps the result to a range from 0 to 255
- and uses the result to set the pulsewidth modulation (PWM) of an output pin.
- Also prints the results to the serial monitor.
-
- The circuit:
- * potentiometer connected to analog pin 0.
-   Center pin of the potentiometer goes to the analog pin.
-   side pins of the potentiometer go to +5V and ground
- * LED connected from digital pin 9 to ground
-
- created 29 Dec. 2008
- modified 9 Apr 2012
- by Tom Igoe
-
- This example code is in the public domain.
-
- */
-
 // These constants won't change.  They're used to give names
 // to the pins used:
 #define N 52 
@@ -37,13 +16,8 @@ int i=0;
 int j=0;
 char code[N]={1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,0,1,0,1,1,0,1,0,0,0,1,1,0,0,1,1,1,1,1,1,1,0,0,0,1,0,0,1,1,1,1,0,1,1,0,0,0};
 
-//void (*StFuns[2]){s0,s1};
 void setup() {
-  // initialize serial communications at 9600 bps:
-  Serial.begin(9600);
-  pinMode(3,INPUT);
   pinMode(5,OUTPUT);
-  pinMode(4,OUTPUT);
   pinMode(13,OUTPUT);
 }
 
@@ -54,32 +28,30 @@ void loop() {
 
   digitalWrite(13,HIGH); //LED
   
-  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
   delayMicroseconds(startbit_low);
   
-  digitalWrite(4,HIGH);
+  digitalWrite(5,HIGH);
   delayMicroseconds(startbit_high);
   
   while (j<N) {
-    digitalWrite(5,pin_state);
-    pin_state=!pin_state;   
     if (code[j]) {
-      digitalWrite(4,LOW);
+      digitalWrite(5,LOW);
       delayMicroseconds(one_logic_low);
-      digitalWrite(4,HIGH);
+      digitalWrite(5,HIGH);
       delayMicroseconds(one_logic_high);
       
     }
     else 
     {
-      digitalWrite(4,LOW);
+      digitalWrite(5,LOW);
       delayMicroseconds(zero_logic_low);
-      digitalWrite(4,HIGH);
+      digitalWrite(5,HIGH);
       delayMicroseconds(zero_logic_high);      
     }
     j++;
   }
-  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
   digitalWrite(13,LOW);  //LED
   delay(100); 
 }
